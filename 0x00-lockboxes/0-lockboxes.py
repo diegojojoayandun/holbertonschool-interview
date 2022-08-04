32d561ea34fa8e487fb3af7e9a74dc69a5ab1691
diegojojoayandun <xcryptorlabs@gmail.com>
2022-08-04 10:45:11 -0500

    fix shebang

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -3,29 +3,14 @@
 """ lockboxes """
 
 
-def joining(T, R):
-    """
-    function to concatenate true and rigth
-    """
-    res = []
-    for e in R:
-        res += T[e]
-    return res
-
-
 def canUnlockAll(boxes):
-    """
-    main function
-    """
-    index = 0
-    total = list(set(boxes[0]) | {0})
-    added = True
-    while added:
-        added = False
-        for j in joining(boxes, total[index:]):
-            if j not in total:
-                total.append(j)
-                index += 1
-                added = True
-
-    return len(total) == len(boxes)
+    unlocked = [0]
+    for box_id, box in enumerate(boxes):
+        if not box:
+            continue
+        for key in box:
+            if key < len(boxes) and key not in unlocked and key != box_id:
+                unlocked.append(key)
+    if len(unlocked) == len(boxes):
+        return True
+    return False
